b9b42697fceeecf88d2e7735f50daed0901846a8
houxieqiang <347557827@qq.com>
2025-03-31 02:36:23 +0800

    sf

--- vue.config.js
+++ vue.config.js
@@ -35,8 +35,8 @@ module.exports = {
     proxy: {
       // editor: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
-        // target: `http://localhost:8080`,
-        target: `http://42.194.230.174:16580`,
+        target: `http://localhost:8080`,
+        // target: `http://42.194.230.174:16580`,
         changeOrigin: true,
         pathRewrite: {
           ['^' + process.env.VUE_APP_BASE_API]: ''
